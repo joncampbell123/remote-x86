908876dd8dbbcd351045047a1907dbdede0957ce
jon <jon@3370dec2-2ef6-11df-8a5b-2f4d88b5036a>
2010-04-02 13:04:21 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/misc/remote-x86@58 3370dec2-2ef6-11df-8a5b-2f4d88b5036a

---
+++ comm/tests/cpu/ac_exception_386-32.asm
@@ -0,0 +1,67 @@
+
+; AC exception test
+;
+; 32-bit protected mode.
+; must be loaded at fixed address 0x40000.
+; this creates and loads it's own because the debugger might
+; be the RS-232 version which doesn't load IDTR. Or, it might
+; be the ethernet version which does.
+;
+; On the 486, this intentionally creates a situation where an
+; Alignment Check exception would occur. We catch it, and note
+; it where the host program can see the results.
+		use32
+		org		0x40000
+
+has:		dd		0
+
+; code begins here at offset 0x0004
+entry:		cli
+		sidt		[idtr]			; save debugger's
+		lidt		[idtr_new]		; load our own
+		
+		mov		esi,[idtr_new+2]	; we want the IDT offset
+
+		; modify INT 17
+		add		esi,17*8
+		mov		ebx,ac_exception
+		mov		word [esi],bx
+		shr		ebx,16
+		mov		ax,cs
+		mov		word [esi+2],ax
+		mov		byte [esi+4],0
+		mov		byte [esi+5],0x8E
+		mov		word [esi+6],bx
+
+		; enable the AC bit
+		pushfd
+		pop		eax
+		or		eax,(1 << 18)
+		push		eax
+		popfd
+
+		; cause an exception
+		mov		ebx,[0x5555]
+
+		; disable the AC bit
+		pushfd
+		pop		eax
+		and		eax,~(1 << 18)
+		push		eax
+		popfd
+
+		; done
+return_path:	lidt		[idtr]			; restore debugger's IDTR
+		ret
+
+idtr:		dw		0,0,0,0
+idtr_new:	dw		0x7FF
+		dd		idt_table
+
+ac_exception:	add		esp,4*3
+		mov		dword [has],0x12345678
+		jmp		return_path
+
+		align		16
+idt_table:
+
